97ecb6d3480769cf2a80734830ef61e0cf96f441
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-06-28 14:32:56 +0600

    basic user login and registration logout

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,7 @@
+from .forms import signupForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
 
 
 def home(request):
@@ -31,4 +31,23 @@ def login_user(request):
 
 
 def logout_user(request):
-    pass
+	logout(request)
+	messages.success(request, "You Have Been Logged Out!")
+	return redirect('home')
+
+
+def register_user(request):
+	if request.method == 'POST':
+		form = signupForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password1')
+			# Authenticate
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Registered Successfully!")
+			return redirect('home')
+	else:
+		form = signupForm()
+	return render(request, 'register.html', {'form': form})
