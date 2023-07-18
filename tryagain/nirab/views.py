469302fd0d7bb9db691fcf95aa4c9fe2cd355420
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-07-19 02:42:53 +0600

    PREPROCESSING USER RATING DATA (found releted drug names to Bangladsh)

--- tryagain/nirab/views.py
+++ tryagain/nirab/views.py
@@ -37,6 +37,7 @@ def get_medication_details(request):
                 'generic_name': medication.generic_name,
                 'manufacturer': medication.manufacturer,
                 'price': str(medication.price),
+                'price_analysis': str(medication.price_analysis),
                 # add any other fields you want to include
             }
             medication_details.append(details)
