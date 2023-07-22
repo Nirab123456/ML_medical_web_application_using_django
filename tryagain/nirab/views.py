21acf8ba7ef7e9914cb62031c6813942b186596c
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-07-22 19:25:10 +0600

    presciption classification based on drug class working ok

--- tryagain/nirab/views.py
+++ tryagain/nirab/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import RegisterForm , addrecord , VenueForm , EventForm , OCRImageForm,Mail_me_Form,profilepicForm,BlogForm,SocialMediaForm,ChangePasswordForm,MedicineForm
-from . models import Record , Event , EventVenue , EventAttendee , RecordImage,Record_mail_me,Post,SocialMedia,Medication,MedicationDetails
+from . models import Record , Event , EventVenue , EventAttendee , RecordImage,Record_mail_me,Post,SocialMedia,Medication,MedicationDetails,Classify_Drug_Class
 import datetime
 import calendar
 from calendar import HTMLCalendar
@@ -73,8 +73,58 @@ def get_medicine_details(request):
     
 
 def get_presciption_classification(request):
-    PRESCIPTION_classification = PRESCIPTION_CLASSIFICATION(request=request)
-    return PRESCIPTION_classification.get_presciption_classification()
+    names = request.GET.get('name', '')  # Get the comma-separated names as a single string
+    names_list = names.split(',')  # Split the string into a list of names
+    generic_name_list = []
+    drug_class_list = []
+    drug_class_details_list = []
+
+    for name in names_list:
+        name = name.lower().strip()  # Use strip() to remove leading/trailing spaces
+        generic_name = Medication.objects.filter(name=name).first()
+        if generic_name:
+            generic_name_list.append(generic_name.generic_name)
+        else:
+            continue
+
+    for name in generic_name_list:
+        drug_class_object = MedicationDetails.objects.filter(generic_name=name).first()
+        if drug_class_object:
+            drug_class = drug_class_object.drug_class
+            drug_class_list.append(drug_class)
+        else:
+            continue
+
+    for drug_class in drug_class_list:
+        drug_class_details = Classify_Drug_Class.objects.filter(drug_class=drug_class)
+        if drug_class_details:
+            for drug_class_detail in drug_class_details:
+                details = {
+                    'name': name,
+                    'group': drug_class_detail.group,
+                    'indication': drug_class_detail.indication,
+                    'drug_class': drug_class_detail.drug_class,
+                    'score': str(drug_class_detail.score),
+                }
+                drug_class_details_list.append(details)
+        else:
+            continue
+
+    if drug_class_details_list:
+        return JsonResponse(drug_class_details_list, safe=False)
+    else:
+        return JsonResponse([], safe=False)
+    
+
+
+
+
+
+
+
+# def get_presciption_classification(request):
+#     PRESCIPTION_classification = PRESCIPTION_CLASSIFICATION(request=request)
+#     return PRESCIPTION_classification.get_presciption_classification()
 
 
 
