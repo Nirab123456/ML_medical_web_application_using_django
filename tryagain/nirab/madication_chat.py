235439aa66faaa2d33ddb555b4b3619c763accb7
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-07-25 17:05:10 +0600

    1st result dictionery priented in console

--- tryagain/nirab/madication_chat.py
+++ tryagain/nirab/madication_chat.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from .forms import MedicineForm
 from .models import MedicationDetails,Medication,Classify_Drug_Class,Classify_Side_Effect,Classify_CONTRADICTIONS
 from django.http import JsonResponse
+from transformers import pipeline
 
 
 
@@ -18,27 +19,45 @@ class MEDICINE_CHAT():
         name = request.GET.get('name')
         name = name.lower()
         generic_name = Medication.objects.filter(name=name).first()
+        topic = request.GET.get('topic')
+        print(topic)
         if generic_name:
             details_of_medicine = MedicationDetails.objects.filter(generic_name=generic_name.generic_name)
             if details_of_medicine.exists():
-                details_list = []
-                for detail in details_of_medicine:
-                    details = {
-                        'generic_name': detail.generic_name,
-                        'drug_class': detail.drug_class,
-                        'indication': detail.indication,
-                        'indication_description': detail.indication_description,
-                        'therapeutic_class_description': detail.therapeutic_class_description,
-                        'pharmacology_description': detail.pharmacology_description,
-                        'dosage_description': detail.dosage_description,
-                        'interaction_description': detail.interaction_description,
-                        'contraindications_description': detail.contraindications_description,
-                        'side_effects_description': detail.side_effects_description,
-                    }
-                    details_list.append(details)
-                return JsonResponse(details_list, safe=False)
-            else:
-                return JsonResponse({'error': 'Medication details not found'}, status=404)
-        else:
-            return JsonResponse({'error': 'Medication not found'}, status=404)
-        
+
+                if topic == 'indication_description':
+                    self.indication_description = details_of_medicine.values_list('indication_description', flat=True)
+
+                    replay = self.init_chat_model(topic=self.indication_description[0])
+                    print(f'indication_replay: {replay}')
+                    print(f'indication_description: {self.indication_description}')
+
+
+
+
+        #         details_list = []
+        #         for detail in details_of_medicine:
+        #             details = {
+        #                 'generic_name': detail.generic_name,
+        #                 'drug_class': detail.drug_class,
+        #                 'indication': detail.indication,
+        #                 'indication_description': detail.indication_description,
+        #                 'therapeutic_class_description': detail.therapeutic_class_description,
+        #                 'pharmacology_description': detail.pharmacology_description,
+        #                 'dosage_description': detail.dosage_description,
+        #                 'interaction_description': detail.interaction_description,
+        #                 'contraindications_description': detail.contraindications_description,
+        #                 'side_effects_description': detail.side_effects_description,
+        #             }
+        #             details_list.append(details)
+        #         return JsonResponse(details_list, safe=False)
+        #     else:
+        #         return JsonResponse({'error': 'Medication details not found'}, status=404)
+        # else:
+        #     return JsonResponse({'error': 'Medication not found'}, status=404)
+
+
+    def init_chat_model(self,topic):
+        tqa = pipeline(task="question-answering",model='deepset/bert-large-uncased-whole-word-masking-squad2',device=0)
+        data = tqa(question="in which cases i can use paracetamol?",context=topic)
+        return data
