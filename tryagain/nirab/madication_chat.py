8cb035a200a93ecf8482feb9c5d744e6e2af446b
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-07-27 21:24:46 +0600

    selected question option primiry beta version done

--- tryagain/nirab/madication_chat.py
+++ tryagain/nirab/madication_chat.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import MedicineForm
-from .models import MedicationDetails,Medication,Classify_Drug_Class,Classify_Side_Effect,Classify_CONTRADICTIONS,med_Ques_Ans
+from .models import MedicationDetails,Medication,Classify_Drug_Class,Classify_Side_Effect,Classify_CONTRADICTIONS,med_Ques_Ans,SELECTED_QUESTION_ANSWER
 from django.http import JsonResponse
 from transformers import pipeline
 
@@ -18,13 +18,16 @@ class MEDICINE_CHAT():
         request = self.request
         names = request.GET.get('name', '')  # Get the comma-separated names as a single string
         names_list = names.split(',')  # Split the string into a list of names
+        self.names_list = names_list
         question = request.GET.get('question')
-        question = question.lower()
-        print(f'type(question): {question}')
         if question == 'null' :
             self.selected_question = request.GET.get('selected_question')
-            print(f'self.selected_question: {self.selected_question}')
+            answer_list = self.get_selected_question()
+            if answer_list:
+                return JsonResponse(answer_list, status=200,safe=False)
+            return JsonResponse({'error': 'Medication details not found'}, status=404)
         else:
+            question = question.lower()
             topic = request.GET.get('topic')
             answer_list = []
 
@@ -85,8 +88,35 @@ class MEDICINE_CHAT():
     def get_selected_question(self):
         question =self.selected_question
         print(f'question: {question}')
+        name_list = self.names_list
+        print(f'name_list: {name_list}')
+        answer_list = []
+        for name in name_list:
+            name = name.lower()
+            generic_name_object = Medication.objects.filter(name=name).first()
+            if generic_name_object is not None:
+                generic_name = generic_name_object.generic_name
+                if generic_name:
+                    question_map = {
+                        'use_cases': 'A_T_U_C',
+                        'side_effects': 'A_S_E_C',
+                        'mechanism_of_action': 'A_M_O_C',
+                        'drug_interactions': 'W_T_A_D_C',
+                        'food_interactions': 'W_T_A_F_C',
+                        'contraindications': 'I_W_C_N_T_T_C',
+                    }
+                    question_field = question_map.get(question)
+                    if question_field:
+                        field_value = SELECTED_QUESTION_ANSWER.objects.filter(generic_name=generic_name).values_list(question_field, flat=True).first()
+                        if field_value:
+                            answer = field_value
+                            answer = answer.replace(generic_name, name)
+                            response_data = {'answer': answer, 'name': name, 'generic_name': generic_name}
+                            answer_list.append(response_data)
+        if answer_list:
+            print(f'answer_list: {answer_list}')
+            return answer_list
 
-    
 
 
 
