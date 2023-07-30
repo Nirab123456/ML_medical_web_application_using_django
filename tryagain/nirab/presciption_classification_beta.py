3ff493223b2c5ccc66ddbdbb00a0178791e460ab
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-07-30 16:05:23 +0600

    got this utput in ser ver side (drug_class_groups,all_mach_groups,all_mach_groups_2,all_matching_uniques)

--- tryagain/nirab/presciption_classification_beta.py
+++ tryagain/nirab/presciption_classification_beta.py
@@ -12,7 +12,11 @@ class PRESCIPTION_CLASSIFICATION_BETA():
 
 
     def get_presciption_classification(self):
-        drug_class_groups,all_mach_groups,all_mach_groups_2=self.re_match_between_matches()
+        drug_class_groups,all_mach_groups,all_mach_groups_2,all_matching_uniques=self.re_match_between_matches()
+        print(f'drug_class_groups: {drug_class_groups}')
+        print(f'all_mach_groups: {all_mach_groups}')
+        print(f'all_mach_groups_2: {all_mach_groups_2}')
+        print(f'all_matching_uniques: {all_matching_uniques}')
         response_data = {}
 
         if drug_class_groups is not None:
@@ -24,7 +28,9 @@ class PRESCIPTION_CLASSIFICATION_BETA():
         if all_mach_groups_2 is not None:
             response_data['all_mach_groups_2'] = all_mach_groups_2
 
-        print(f'response_data: {response_data["drug_class_groups"]}')
+        if all_matching_uniques is not None:
+            response_data['all_matching_uniques'] = all_matching_uniques
+
         if response_data:
             return JsonResponse(response_data, safe=False)
         else:
@@ -81,6 +87,7 @@ class PRESCIPTION_CLASSIFICATION_BETA():
         drug_class_groups_list = self.get_drug_class_classification()
         all_list = []
         all_matching_list = []
+        all_combined_list = []
 
         if drug_class_groups_list is not None:
             for drug_class_group in drug_class_groups_list:
@@ -111,10 +118,56 @@ class PRESCIPTION_CLASSIFICATION_BETA():
                         'specific_class_matches': list(specific_class_matches)
                     })
 
-        return all_matching_list,drug_class_groups_list
+
+        name=[]
+        headings=[]
+        specific_class=[]
+        #get the unique heading_matches ,specific_class_matches
+        for all_matching in all_matching_list:
+            name1 = all_matching['name1']
+            name2 = all_matching['name2']
+            heading_matches = all_matching['heading_matches']
+            specific_class_matches = all_matching['specific_class_matches']
+            name.append(name1)
+            name.append(name2)
+            headings.append(heading_matches)
+            specific_class.append(specific_class_matches)
+
+
+        name=list(set(name))
+
+
+        headings = [tuple(sublist) for sublist in headings]
+        headings=list(set(headings))
+        headings = [item[0] for item in headings]
+
+
+
+        specific_class = [tuple(sublist) for sublist in specific_class]
+        specific_class=list(set(specific_class))
+        specific_class = [item[0] for item in specific_class if item]
+
+
+
+        all_combined_list.append(name)
+        all_combined_list.append(headings)
+        all_combined_list.append(specific_class)
+
+
+
+
+
+
+
+        return all_matching_list,drug_class_groups_list,all_combined_list
     
+
+
+
+
+
     def re_match_between_matches(self):
-        all_matching_list,drug_class_groups_list = self.calculate_drug_class_match()
+        all_matching_list,drug_class_groups_list,all_combined_list = self.calculate_drug_class_match()
         all_list = []
         all_matching_list2 = []
 
@@ -141,16 +194,38 @@ class PRESCIPTION_CLASSIFICATION_BETA():
 
                 # If there are matches, add them to the all_matching_list
                 if heading_matches or specific_class_matches:
-                    all_matching_list2.append({
-                        'name1': name1,
-                        'name2': name2,
-                        'name3': name3,
-                        'name4': name4,
-                        'heading_matches': list(heading_matches),
-                        'specific_class_matches': list(specific_class_matches)
-                    })
+                    if name1 == name3 or name1 == name4 or name2 == name3 or name2 == name4:
+                        all_matching_list2.append({
+                            'name1': name1,
+                            'name2': name2,
+                            'heading_matches': list(heading_matches),
+                            'specific_class_matches': list(specific_class_matches)
+                        })
 
-        return drug_class_groups_list,all_matching_list,all_matching_list2
+                    else:
+                        all_matching_list2.append({
+                            'name1': name1,
+                            'name2': name2,
+                            'name3': name3,
+                            'name4': name4,
+                            'heading_matches': list(heading_matches),
+                            'specific_class_matches': list(specific_class_matches)
+                        })
+        #remove duplicates
+        all_matching_list2 = [i for n, i in enumerate(all_matching_list2) if i not in all_matching_list2[n + 1:]]
+        return drug_class_groups_list,all_matching_list,all_matching_list2,all_combined_list
+                            
+
+        #             all_matching_list2.append({
+        #                 'name1': name1,
+        #                 'name2': name2,
+        #                 'name3': name3,
+        #                 'name4': name4,
+        #                 'heading_matches': list(heading_matches),
+        #                 'specific_class_matches': list(specific_class_matches)
+        #             })
+
+        # return drug_class_groups_list,all_matching_list,all_matching_list2
     
 
 
