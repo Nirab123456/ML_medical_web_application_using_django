30f434d0754fa67705bc60841e3957def9bf2252
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-08-06 15:43:50 +0600

    safely seperated html and js(now will work with medication nav)

--- tryagain/about_me/js/search_results.js
+++ tryagain/about_me/js/search_results.js
@@ -1,3 +1,5 @@
+
+
   // Retrieve the query parameters from the URL
   const urlParams = new URLSearchParams(window.location.search);
   const strength = urlParams.get('strength');
@@ -6,99 +8,104 @@
   
   // Now you have the values of the query parameters in the variables (strength, dosageForm, drugName).
   // You can use these values as needed in the JavaScript code of this HTML page.
-  console.log('Strength:', strength);
-  console.log('Dosage Form:', dosageForm);
-  console.log('Drug Name:', drugName);
+  // console.log('Strength:', strength);
+  // console.log('Dosage Form:', dosageForm);
+  // console.log('Drug Name:', drugName);
 
 
 
 
-document.getElementById("sortOption").addEventListener("change", handleSortOptionChange);
 
-function handleSortOptionChange() {
-  var selectedSortOption = document.getElementById("sortOption").value;
-  // Call the function to fetch medication details with the updated sort option
-  fetchMedicationDetails(selectedSortOption);
-}
+var currentPage = 1; // The current page number
+var medicationDetails = []; 
 
-
-function fetchMedicationDetails(sortOption) {
-  // Make an AJAX call to fetch the medication details based on the selected strength, dosage form, and drug name
+function fetchMedicationDetails(sortOption, currentPage) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', '/get_medication/?strength=' + strength + '&dosage_form=' + dosageForm + '&name=' + drugName, true);
-  // xhr.open('GET', '/get_medication/?strength=' + strength + '&dosage_form=' + dosageForm + '&name=' + drugName + '&sort=' + sortOption, true);
 
-  xhr.onload = function() {
+  xhr.onload = function () {
     if (xhr.status === 200) {
-      var medicationDetails = JSON.parse(xhr.responseText);
+      medicationDetails = JSON.parse(xhr.responseText); // Assign the fetched medication details to the global variable
+
       if (Array.isArray(medicationDetails) && medicationDetails.length > 0) {
         // Sort the medication details based on the selected sort option
-        
-        if (sortOption === "priceAsc") {
-          medicationDetails.sort(function(a, b) {
-            return a.price_analysis - b.price_analysis;
-          });
-        } else if (sortOption === "priceDesc") {
-          medicationDetails.sort(function(a, b) {
-            return b.price_analysis - a.price_analysis;
-          });
-        }
-        
-        // Display details for each medication
-        var detailsHtml = "";
-        for (var i = 0; i < medicationDetails.length; i++) {
-          var medication = medicationDetails[i];
-          detailsHtml += '<div class="card card-margin">';
-          detailsHtml += '  <div class="card-body">';
-          detailsHtml += '    <div class="row search-body">';
-          detailsHtml += '      <div class="col-lg-12">';
-          detailsHtml += '        <div class="search-result">';
-          detailsHtml += '          <div class="result-body">';
-          detailsHtml += '            <div class="table-responsive">';
-          detailsHtml += '              <table class="table  widget-26">';
-          detailsHtml += '                <tbody>';
-          detailsHtml += '                  <tr>';
-          detailsHtml += '                    <td class="column-1">';
-          detailsHtml += '                      <div class="widget-26-job-title">';
-          detailsHtml += '                        <a href="#">' + medication.name.trim() + '</a>';
-          detailsHtml += '                        <p class="m-0">';
-          detailsHtml += '                          <a href="#" class="employer-name text-center Class">' + medication.dosage_form.trim() + '.</a>';
-          detailsHtml += '                        </p>';
-          detailsHtml += '                      </div>';
-          detailsHtml += '                    </td>';
-          detailsHtml += '                    <td class="column-2" >';
-          detailsHtml += '                      <div class="widget-26-job-info">';
-          detailsHtml += '                        <p class="type m-0">' + medication.manufacturer.trim() + '</p>';
-          detailsHtml += '                        <a  class="type m-0 employer-name">' + medication.generic_name.trim() + '.</a>';
-          detailsHtml += '                      </div>';
-          detailsHtml += '                    </td>';
 
-          if(medication.price != '0'){
-            detailsHtml += '                    <td class="column-3" >';
-            detailsHtml += '                      <div class="widget-26-job-category">';
-            detailsHtml += '                        <p class="m-0 text-center Class">' + medication.price.trim() + '</p>';
-            detailsHtml += '                      </div>';
-            detailsHtml += '                    </td>';
+        // console.log(currentPage)
+
+        var startIndex = (currentPage - 1) * 10;
+        var endIndex = startIndex + 10;
+        
+        if (startIndex < 0) {
+          startIndex = 0; // Ensure startIndex is not negative
+        }
+
+        if (endIndex > medicationDetails.length) {
+          endIndex = medicationDetails.length; // Ensure endIndex is within bounds
+        }
+          // Create a copy of the subset of medication details to be sorted
+          var medicationSubset = medicationDetails.slice(startIndex, endIndex);
+
+          if (sortOption === "priceAsc") {
+            medicationSubset.sort(function (a, b) {
+              return a.price_analysis - b.price_analysis;
+            });
+          } else if (sortOption === "priceDesc") {
+            medicationSubset.sort(function (a, b) {
+              return b.price_analysis - a.price_analysis;
+            });
           }
-            else{
-            detailsHtml += '                    <td class="column-3">';
-            detailsHtml += '                      <div class="widget-26-job-category">';
-            detailsHtml += '                        <p class="m-0 text-center Class">PRICE DATA IS UNAVAILABLE</p>';
-            detailsHtml += '                      </div>';
-            detailsHtml += '                    </td>';
-            }
 
-          detailsHtml += '                  </tr>';
-          detailsHtml += '                </tbody>';
-          detailsHtml += '              </table>';
-          detailsHtml += '            </div>';
-          detailsHtml += '          </div>';
-          detailsHtml += '        </div>';
-          detailsHtml += '      </div>';
-          detailsHtml += '    </div>';
-          detailsHtml += '  </div>';
-          detailsHtml += '</div>';
-        }
+        var detailsHtml = "";
+        for (var i = 0; i < medicationSubset.length; i++) {
+          var medication = medicationSubset[i];
+
+          detailsHtml +=
+            `
+          <div class="card card-margin">
+            <div class="row row-space">
+              <div  class="my_coll" >
+                <table class="table  widget-26">
+                  <tbody>
+                    <tr>
+                      <td>
+                        <div class="widget-26-job-title">
+                          <a href="#">${medication.name.trim()}</a>
+                          <p class="m-0">
+                            <a href="#">${medication.dosage_form.trim()}.</a>
+                          </p>
+                        </div>
+                      </td>
+                      <td class="column-2">
+                        <div class="widget-26-job-info">
+                          <p class="type m-0">${medication.manufacturer.trim()}</p>
+                          <a class="type m-0 employer-name">${medication.generic_name.trim()}.</a>
+                        </div>
+                      </td>`;
+          if (medication.price !== '0') {
+            detailsHtml += `<td class="column-3">
+                              <div>
+                                <p class="m-0 text-center Class">PRICE: ${medication.price.trim()} </p>
+                              </div>
+                            </td>`;
+          } else {
+            detailsHtml += `<td class="column-3">
+                              <div class="widget-26-job-category">
+                                <p class="m-0 text-center Class">PRICE DATA IS UNAVAILABLE</p>
+                              </div>
+                            </td>`;
+          }
+          detailsHtml += `</tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+                `;
+        
+        
+        
+
+              }
         document.getElementById("medicationDetails").innerHTML = detailsHtml;
       } else {
         // Display "No data found" message
@@ -110,12 +117,63 @@ function fetchMedicationDetails(sortOption) {
       document.getElementById("medicationDetails").innerHTML = "<p>No data found</p>";
     }
   };
-  xhr.onerror = function() {
+  xhr.onerror = function () {
     console.error('Request failed. Network error');
     // Display "No data found" message for network errors
     document.getElementById("medicationDetails").innerHTML = "<p>No data found</p>";
   };
   xhr.send();
 }
-fetchMedicationDetails("default");
 
+function calculateTotalPages(items) {
+  var itemsPerPage = 10;
+  var totalItems = items.length;
+  return Math.ceil(totalItems / itemsPerPage);
+}
+
+function displayPage(pageNumber) {
+  showPage(pageNumber);
+}
+
+var trackpage=0;
+function showPage(pageNumber) {
+  var totalPages = calculateTotalPages(medicationDetails);
+
+  if(pageNumber===2){
+    
+    trackpage += pageNumber;
+    pageNumber =1 + Math.floor(trackpage / 2);
+    if(pageNumber>totalPages){
+      pageNumber=totalPages;
+      trackpage=totalPages*2-2;
+    }
+
+  }
+  else if(pageNumber===0)
+  {
+
+    trackpage -= 2;
+    pageNumber =1+ Math.floor(trackpage / 2);
+    if(pageNumber<1){
+      pageNumber=1;
+      trackpage=0;
+    }
+
+  }
+
+
+document.getElementById("sortOption").addEventListener("change", handleSortOptionChange);
+
+  function handleSortOptionChange() {
+  var sortOption = document.getElementById("sortOption").value;
+  return sortOption;
+}
+
+  if (pageNumber >= 1 && pageNumber <= totalPages) {
+    var sortOption = handleSortOptionChange();
+    fetchMedicationDetails(sortOption, pageNumber);
+  }
+}
+
+// Call this after fetching medicationDetails
+fetchMedicationDetails('default', 1);
