2e3b813a166c94cc2ddaf1ca247dae2177075903
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-08-16 10:28:11 +0600

    bsic html css fixed of all the functions (except text to handwritten all working like a cherm)

--- tryagain/nirab/eng_ocr.py
+++ tryagain/nirab/eng_ocr.py
@@ -127,7 +127,7 @@ class ENGOCR:
 
 
                     if file_path:
-                        return render(request, 'base_handwritten.html', {'ENG_OCR_form': form, 'ENG_OCR_image_url': existing_record.image.url, 'ENG_OCR_text': text, 'ENG_OCR_file_path': file_path , 'ENG_OCR_pdf_output_file':pdf_output_file})
+                        return render(request, 'base_ocr.html', {'ENG_OCR_form': form, 'ENG_OCR_image_url': existing_record.image.url, 'ENG_OCR_text': text, 'ENG_OCR_file_path': file_path , 'ENG_OCR_pdf_output_file':pdf_output_file})
                 
                 
                 
@@ -183,10 +183,12 @@ class ENGOCR:
                         os.remove(file_name)
 
                     pdf_output_file = output_file
+                    print('text path',file_path)
+                    print('pdf path',pdf_output_file)
 
 
                     if file_path:
-                        return render(request, 'base_handwritten.html', {'ENG_OCR_form': form, 'ENG_OCR_image_url': record_image.image.url, 'ENG_OCR_text': text, 'ENG_OCR_file_path': file_path, 'ENG_OCR_pdf_output_file':pdf_output_file})
+                        return render(request, 'base_ocr.html', {'ENG_OCR_form': form, 'ENG_OCR_image_url': record_image.image.url, 'ENG_OCR_text': text, 'ENG_OCR_file_path': file_path, 'ENG_OCR_pdf_output_file':pdf_output_file})
         else:
             form = OCRImageForm()
-        return render(request, 'base_handwritten.html', {'ENG_OCR_form': form})
+        return render(request, 'base_ocr.html', {'ENG_OCR_form': form})
