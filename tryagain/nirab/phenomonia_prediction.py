840b23bc4121bd06f807f74a6fad48811a119633
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-08-27 13:18:59 +0600

    getting image passing it to webpage and saving it to database

--- tryagain/nirab/phenomonia_prediction.py
+++ tryagain/nirab/phenomonia_prediction.py
@@ -27,14 +27,16 @@ class PHENOMONIA_PREDICTION_CLASS():
         if request.method == 'POST':
             form = phenomonia_prediction_form(request.POST, request.FILES)
             if form.is_valid():
-                #get image from form
+                #get image from form and pass it to web page
                 image = form.cleaned_data['image']
-                print(image)
-                #open image
-                img = Image.open(image)
-            return render(request, 'C_V_D_prediction.html', {'X_P_prediction_form': form, 'X_P_prediction_img': img})
+                #save image to database
+                PHENOMONIA_PREDICTION.objects.create(user=request.user,image=image)
+                #get image from database
+                image = PHENOMONIA_PREDICTION.objects.filter(user=request.user).last()
+                #get image path
+                image_path = image.image.url
+                return render(request, 'C_V_D_prediction.html', {'form': form,'image_path':image_path})
         else:
             form = phenomonia_prediction_form()
-            return render(request, 'C_V_D_prediction.html', {'X_P_prediction_form': form})
-
+        return render(request, 'C_V_D_prediction.html', {'form': form})
 
